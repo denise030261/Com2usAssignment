ecb06bf3c69a0d8de7d40b808563afc38cde3263
denise030261 <83278071+denise030261@users.noreply.github.com>
2024-03-09 01:27:02 +0900

    컴썰 UI 플레이어 미니맵 추가

---
+++ Assets/Scripts/Player/MniPlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MniPlayer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    
+    RectTransform rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.anchoredPosition = new Vector2(player.transform.position.x*(43.0f/23.3f), player.transform.position.y*3);
+    }
+}
